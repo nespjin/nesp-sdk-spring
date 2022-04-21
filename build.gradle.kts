8829ca7341b2d708e2e0e0fd510d3b2eed00b1da
Jinzhaolu <1756404649@qq.com>
2022-04-21 18:12:39 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm")/* version "1.6.20"*/
 }
 
 group = "com.nesp"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven {
+        setUrl("https://maven.aliyun.com/nexus/content/groups/public/")
+    }
+    maven {
+        setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter")
+    }
+
+    maven { url = uri("https://jitpack.io") }
     mavenCentral()
+
 }
 
 dependencies {
