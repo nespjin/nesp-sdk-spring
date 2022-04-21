6ce5458c5e779fc96246742c1235938239237c66
靳兆鲁 <1756404649@qq.com>
2022-04-22 01:11:10 +0800

    Update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,14 @@ group = "com.nesp"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven {
-        setUrl("https://maven.aliyun.com/nexus/content/groups/public/")
-    }
-    maven {
-        setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter")
-    }
-
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public/") }
+    maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
     maven { url = uri("https://jitpack.io") }
-    mavenCentral()
 
+    gradlePluginPortal()
+    google()
+    mavenCentral()
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+include(":nesp-sdk-java")
+include(":nesp-sdk-kotlin")
+
+project(":nesp-sdk-java").projectDir = File("nesp-sdk-java/nesp-sdk-java")
+project(":nesp-sdk-kotlin").projectDir = File("nesp-sdk-kotlin/nesp-sdk-kotlin")
 
 rootProject.name = "nesp-sdk-spring"
-
